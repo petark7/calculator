85bafc86233bbf29770fa49b712a6372f8d38c30
petark7 <kuzmanovski.petar@uklo.edu.mk>
2022-11-12 20:39:08 +0100

    First working version
    All operations are working correctly.

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
     let display = document.querySelector('.display h2');
     let buttons = document.querySelectorAll('button');
+    let clearDisplay = true;
 
+    let currentNumber = "";
+    let currentOperand = "";
+    let numbersArray = [];
+
+    // populate display on button click
     for (let x = 0; x < buttons.length; x++) {
         buttons[x].addEventListener('click', () => {
-            if (display.textContent == 0) {
+            if (display.textContent == 0 && !buttons[x].className) {
                 display.textContent = "";
             }
-            display.textContent += buttons[x].textContent;
+            // check if button should render in display
+            // numbers render in display (have no classnames)
+                if (!buttons[x].className) {
+                    if (clearDisplay) {
+                        currentNumber = "";
+                        display.textContent = "";
+                    }
+                    currentNumber += buttons[x].textContent;
+                    display.textContent += buttons[x].textContent;     
+                    clearDisplay = false;  
+                }
         });
     }
 
-function buttonClick (content) {
-    display.textContent += content;
+function calculate (operand) {
+    console.log(numbersArray);
+    if (operand == "=") {
+        numbersArray.push(parseInt(currentNumber));
+        let result = operate(numbersArray[0], numbersArray[2], numbersArray[1]);
+        display.textContent = result;
+        numbersArray = [];
+    }
+
+    // operands + - * /
+    else {
+        // executes after the display is cleared (or the first time)
+        if (!numbersArray.length) {
+            numbersArray.push(parseInt(currentNumber));
+            numbersArray.push(operand); 
+        }
+    }
+    clearDisplay = true;
 }
 
 function add (x, y) {
